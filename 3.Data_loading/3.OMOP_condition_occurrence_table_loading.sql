6d14b41ab748e21311c9e84e25bde62253fec1d6
yuey11 <yuey11jlu@gmail.com>
2018-08-20 15:01:41 -0500

    Comments Update

---
+++ 3.Data_loading/3.OMOP_condition_occurrence_table_loading.sql
@@ -0,0 +1,4 @@
+--Loading the condition_occurrence table data of public_temp schema to formal OHDSI CDM database.
+--The public schema is the formal OHDSI CDM database.
+--All the data in temporary schema could be inputted into the OHDSI CDM table whether there have been some data in the database or not.
+
